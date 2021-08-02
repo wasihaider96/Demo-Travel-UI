5cb6e60833d2bdfd7103eeba7fdf81e93456eb62
ili Digital <ali@ili.digital>
2021-08-02 15:06:47 +0500

    project setup

---
+++ Travel/Travel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel
+//
+//  Created by Dev ILI on 02/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
